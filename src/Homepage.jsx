47c9bd757ccb4f027249c98f7cb02301371259f5
Des G Bauza <dgb.javadeveloper@gmail.com>
2024-01-11 14:04:14 -0500

    Turning all functional components into classes

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,20 +1,33 @@
 
 
-    // Questions: 
-    // - How do I reference three (3) 'Assignment' objects from the backend that need to be displayed? Can I pass it in thru a class declaration or axios call?
-    // - What does an Axios call return? How do we know an axios call failed to do action? 
-    // - How does App instantiate and use the different components?
-
-    /**
-     * Axios Calls : 
-     *  
-     * 
+     /* 
+     * const response = axios.get();
+     *      Array of responses 
+     *      Show all data 
+     *      - State of assignment 
      */
 
-    function HomePage(){
+     // Add into return statement a div 
+     // <div> getAssignment(); 
+     // this.getAssignmentList.map(assignment => Assignment data = {assignment} </div> )
 
-        return (
-         
+    class HomePage extends React.Component {
+
+        constructor(){
+            this.state = {
+                assignment = null;
+            };
+
+        }
+
+        getAssignment(){
+            axios.get()
+            .then(response => this.setState(assignmentList : response.data))
+        }
+        render(){
+            return (
+
+        
             <div className="container">
 
             <div className="header">
@@ -22,6 +35,8 @@
             </div>
         
             <div className="flexbox-container"> 
+
+             
                 <div> Assignment1</div> 
                 <div> Assignment2</div>
                 <div> Assignment3 </div>
@@ -34,45 +49,13 @@
             </div>
        
 
-        );
-
+       );
+     }
+        
 
     }
 
-    // Can I pass a running instance of Assignment object in through the class like this? 
-
-    /**
-     * class HomePage extends React.Component {
-     *      constructor(Assignment assignment, Assignment assignment, Assignment assignment){
-     *          super();
-     *          this.state = { Assignment assignment, Assignment assignment, Assignment assignment };
-     *       render(){
-     *             return   
-     *          
-     *              <div className="container">
-
-            <div className="header">
-                <h1> Welcome to the Assignment Review App </h1>
-            </div>
-        
-            <div className="flexbox-container"> 
-                <div> Assignment1</div> 
-                <div> Assignment2</div>
-                <div> Assignment3 </div>
-            </div>
-        
-            <div className="bottom">
-                <button> Go to dashboard</button>
-            </div>
-        
-            </div>
-       
-     * 
-     *      }
-     *      }
-     * }
-     */
-
+   
    
     export default HomePage;
 
